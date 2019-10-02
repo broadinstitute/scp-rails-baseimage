3d4f20f482dee203d427361371ac9b95df0a2d54
Scott Longley <slongley@broadinstitute.org>
2019-10-02 16:38:12 -0400

    temp debug messages

--- ci/jenkins.build-and-test.bash
+++ ci/jenkins.build-and-test.bash
@@ -6,6 +6,16 @@ THIS_DIR="$(cd "$(dirname "$BASH_SOURCE")"; pwd)"
 BASE_DIR="$(dirname $THIS_DIR)"
 BASH_INCLUDE="$BASE_DIR/lib/bash_include"
 
+echo DEBUG: ls $BASE_DIR
+ls $BASE_DIR
+echo DEBUG: ls $BASE_DIR/lib
+ls $BASE_DIR/lib
+echo DEBUG: ls $BASE_DIR/lib/bash_include
+ls $BASE_DIR/lib/bash_include
+echo DEBUG: ls $BASH_INCLUDE
+ls $BASH_INCLUDE
+echo DEBUG: ls $BASH_INCLUDE/bash_utils.bash
+ls $BASH_INCLUDE/bash_utils.bash
 . $BASH_INCLUDE/bash_utils.bash
 
 $THIS_DIR/build || exit_with_error_message "build failed (at $BASH_SOURCE:$LINENO)"
