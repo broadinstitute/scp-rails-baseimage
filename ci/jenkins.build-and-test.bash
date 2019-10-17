a04a5eed6c7568184e0c1d36addda973b081dcde
Scott Longley <slongley@broadinstitute.org>
2019-10-17 15:03:55 -0400

    attempting to simplify

--- ci/jenkins.build-and-test.bash
+++ ci/jenkins.build-and-test.bash
@@ -2,7 +2,6 @@
 
 # USAGE: Use without arguments in a jenkins test job to build and test the image. This keeps jenkins job config complexity low.
 
-echo "DEBUG: (in jenkins.build-and-test.bash) \$BASH_SOURCE is \"$BASH_SOURCE\""
 THIS_DIR="$(cd "$(dirname "$BASH_SOURCE")"; pwd)"
 BASE_DIR="$(dirname $THIS_DIR)"
 BASH_INCLUDE="$BASE_DIR/lib/bash_include"
