675c77d58d2c20b99e7f991aad4f8a8eb12c08f8
bistline <bistline@broadinstitute.org>
2020-08-27 13:28:28 -0400

    rolling back accidental commit to master

--- requirements.bash
+++ requirements.bash
@@ -2,7 +2,7 @@ BASE_IMAGE="marketplace.gcr.io/google/ubuntu1804:latest" #aggressively keep up w
 
 # pin to versioned releases of phusion images to avoid breakages
 PHUSION_BASE_IMAGE_REPO="phusion/baseimage-docker"
-PHUSION_BASE_IMAGE_VERSION="bionic-1.0.0"
+PHUSION_BASE_IMAGE_VERSION="0.11"
 
 PHUSION_PASSENGER_IMAGE_REPO="phusion/passenger-docker"
-PHUSION_PASSENGER_IMAGE_VERSION="1.0.11" #if you change this, you'll have to change the "FROM" line in ./Dockerfile also.
+PHUSION_PASSENGER_IMAGE_VERSION="1.0.9" #if you change this, you'll have to change the "FROM" line in ./Dockerfile also.
