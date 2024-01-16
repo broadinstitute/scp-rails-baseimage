2086d0b00925d591a89127d61e32d2eb7dd540ea
bistline <bistline@broadinstitute.org>
2024-01-16 11:20:48 -0500

    refactoring some tasks for GHA only

---
+++ ci/gha.build-and-test.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# USAGE: Use without arguments in a jenkins test job to build and test the image. This keeps jenkins job config complexity low.
+
+THIS_DIR="$(cd "$(dirname "$BASH_SOURCE")"; pwd)"
+BASE_DIR="$(dirname $THIS_DIR)"
+BASH_INCLUDE="$BASE_DIR/lib/bash_include"
+
+. $BASH_INCLUDE/show_banner.bash
+. $BASH_INCLUDE/bash_utils.bash
+
+$THIS_DIR/clean || exit_with_error_message "clean FAILED" # DEBUG_HACK: For faster debugging when running this repeately on a dev workstation for DEBUGGING, you may want to comment this line out.
+$THIS_DIR/gha-build || exit_with_error_message "build FAILED"
+$THIS_DIR/test  || exit_with_error_message "test FAILED"
+$THIS_DIR/check_for_newer_dependencies || { echo "WARNING: Setting jenkins job to unstable to trigger email for new dependency versions that this build has not adopted yet..." >&2; exit 99; }
