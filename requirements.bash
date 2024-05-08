346b12b99ef5672b548f9cb72cab7811a0117067
bistline <bistline@broadinstitute.org>
2024-05-08 11:21:57 -0400

    updating to phusion passenger 2.5.1

--- requirements.bash
+++ requirements.bash
@@ -5,4 +5,4 @@ PHUSION_BASE_IMAGE_REPO="phusion/baseimage-docker"
 PHUSION_BASE_IMAGE_VERSION="focal-1.2.0"
 
 PHUSION_PASSENGER_IMAGE_REPO="phusion/passenger-docker"
-PHUSION_PASSENGER_IMAGE_VERSION="2.4.1" #if you change this, you'll have to change the "FROM" line in ./Dockerfile also.
+PHUSION_PASSENGER_IMAGE_VERSION="2.5.1" #if you change this, you'll have to change the "FROM" line in ./Dockerfile also.
