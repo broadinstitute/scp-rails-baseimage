8e569c17ef8a5bfe8b20f08e9e315edbf0c331cc
bistline <bistline@broadinstitute.org>
2025-07-23 16:10:59 -0400

    Updating to phusion_passenger:3.1.3 and Ruby 3.4.4

--- requirements.bash
+++ requirements.bash
@@ -5,4 +5,4 @@ PHUSION_BASE_IMAGE_REPO="phusion/baseimage-docker"
 PHUSION_BASE_IMAGE_VERSION="noble-1.0.0"
 
 PHUSION_PASSENGER_IMAGE_REPO="phusion/passenger-docker"
-PHUSION_PASSENGER_IMAGE_VERSION="3.1.2" #if you change this, you'll have to change the "FROM" line in ./Dockerfile also.
+PHUSION_PASSENGER_IMAGE_VERSION="3.1.3" #if you change this, you'll have to change the "FROM" line in ./Dockerfile also.
