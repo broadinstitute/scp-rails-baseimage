d10f1dd5b475a8323eb90029c68d4450c62f3e27
bistline <bistline@broadinstitute.org>
2025-07-24 12:31:51 -0400

    updating versions re: ruby not updating

--- requirements.bash
+++ requirements.bash
@@ -2,7 +2,7 @@ BASE_IMAGE="marketplace.gcr.io/google/ubuntu2404:latest" #aggressively keep up w
 
 # pin to versioned releases of phusion images to avoid breakages
 PHUSION_BASE_IMAGE_REPO="phusion/baseimage-docker"
-PHUSION_BASE_IMAGE_VERSION="noble-1.0.0"
+PHUSION_BASE_IMAGE_VERSION="noble-1.0.1"
 
 PHUSION_PASSENGER_IMAGE_REPO="phusion/passenger-docker"
 PHUSION_PASSENGER_IMAGE_VERSION="3.1.3" #if you change this, you'll have to change the "FROM" line in ./Dockerfile also.
